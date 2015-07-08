7b1d17e5078e113043a3c81a9aa0cce82317d837
HashBang173 <hashbang173@gmail.com>
2015-07-08 18:03:36 -0400

    7/8/15
    Compiled on Fedora 22

--- include/isl/stdint.h
+++ include/isl/stdint.h
@@ -2,7 +2,7 @@
 #define _ISL_INCLUDE_ISL_STDINT_H 1
 #ifndef _GENERATED_STDINT_H
 #define _GENERATED_STDINT_H "isl 0.14"
-/* generated using gnu compiler x86_64-linux-gnu-gcc (Ubuntu 4.9.2-10ubuntu13) 4.9.2 */
+/* generated using gnu compiler gcc (GCC) 5.1.1 20150618 (Red Hat 5.1.1-4) */
 #define _STDINT_HAVE_STDINT_H 1
 #include <stdint.h>
 #endif
